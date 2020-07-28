cede95472432f7549d893e9f92b8d41b4c1de88e
Amirul Asyraf <58986949+asyraf-labs@users.noreply.github.com>
2020-07-28 20:23:05 +0800

    delete the credentials in config files

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAdO44Jq68npc-OorZYvp_m-umiYd23BnI",
-    authDomain: "instagram-clone-react-97c56.firebaseapp.com",
-    databaseURL: "https://instagram-clone-react-97c56.firebaseio.com",
-    projectId: "instagram-clone-react-97c56",
-    storageBucket: "instagram-clone-react-97c56.appspot.com",
-    messagingSenderId: "408064452718",
-    appId: "1:408064452718:web:aeb59f28aa2ed356491e7f",
-    measurementId: "G-SHPP51WM67"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 const db = firebaseApp.firestore();
